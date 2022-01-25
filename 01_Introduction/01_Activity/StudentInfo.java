d83a7fc55d52450df147f9a6328c5f22b9e5d42b
Jack Cwynar <89153985+jcwynar@users.noreply.github.com>
2022-01-25 11:24:04 -0600

    Add files via upload

---
+++ 01_Introduction/01_Activity/StudentInfo.java
@@ -0,0 +1,23 @@
+/**
+ * Simple program to print info
+ * about myself as a student.
+ *
+ * Activity 01
+ * @author Jack Cwynar COMP 1210-006
+ * @version 1/13/20
+ */
+ 
+public class StudentInfo {
+  /**
+   * Prints my information.
+   * @param args Comman line arguments - not used.
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Jack Cwynar");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   ENGR 1110");
+   
+   }
+
+}
